0256829fd8ce6771dda347e0cc026323ee7a4946
Akash Kulkarni <akash213kulkarni@gmail.com>
2020-09-05 22:31:03 +0530

    Updates

---
+++ precache-manifest.88d9cd6bdc97ae759503429cd0cc1657.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0c16cbb7ebbefdb072daec267c183b6",
+    "url": "/React-todolist/index.html"
+  },
+  {
+    "revision": "a83e7564ea5d2073af37",
+    "url": "/React-todolist/static/css/main.219893a7.chunk.css"
+  },
+  {
+    "revision": "23b311132f5c4b6792df",
+    "url": "/React-todolist/static/js/2.bbf527c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-todolist/static/js/2.bbf527c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a83e7564ea5d2073af37",
+    "url": "/React-todolist/static/js/main.fad23ff0.chunk.js"
+  },
+  {
+    "revision": "9bdfa64be2bc94bf3a5a",
+    "url": "/React-todolist/static/js/runtime-main.89df498e.js"
+  }
+]);
